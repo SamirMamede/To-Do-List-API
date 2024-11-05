6e64de26a8a342aa56b46b83a130f31f91fe10d0
SamirMamede <anuarsamir@gmail.com>
2024-11-04 23:08:04 -0400

    Adds the task_detail function to handle GET, PUT, and DELETE requests for specific tasks.

--- todolist/api/urls.py
+++ todolist/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_tasks, create_task
+from .views import get_tasks, create_task, task_detail
 
 urlpatterns = [
     path('tasks/', get_tasks, name='get_tasks'),
     path('tasks/create/', create_task, name='create_task'),
+    path('tasks/<int:pk>/', task_detail, name='task_detail'),
 ]
 
